9e7a9527a1a52f0dfa4d12dd3313aa45c5dcf5b9
Hoony0321 <eogns0321@gmail.com>
2025-03-27 11:59:03 -0700

    feat : add inter font and apply global padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,23 @@
 import { Provider } from "@/src/components/ui/provider";
+import { Inter } from "next/font/google";
+const inter = Inter({
+  weight: ["400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html suppressHydrationWarning>
+    <html suppressHydrationWarning className={inter.className}>
         <body>
         <Provider>
           <div style={{ width: "100%" }}>
-             
-              {children}
+            <div style={{padding: "20px"}}>
+                {children}
+            </div>
           </div>
         </Provider>
       </body>
